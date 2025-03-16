a6d96bb3faf95a3a1d51b89fa8d32f16146543e6
Cody Seavey <cody@seavey.dev>
2025-03-16 00:19:59 -0500

    Replace NerdTree with NeoTree and enable debugging, gitsigns and switch the theme from monokai to github dark

--- init.lua
+++ init.lua
@@ -335,6 +335,7 @@ require('lazy').setup({
         { '<leader>d', group = '[D]ocument' },
         { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch' },
+        { '<leader>n', group = '[N]eotree' },
         { '<leader>w', group = '[W]orkspace' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
@@ -1001,28 +1002,6 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
-  {
-    'preservim/nerdtree', -- File system explorer'
-    keys = {
-      {
-        '<leader>n',
-        function()
-          vim.cmd 'NERDTreeToggle'
-        end,
-        mode = 'n',
-        desc = '[N]ERDTree',
-      },
-      config = function()
-        -- Start NERDTree when Vim starts with a directory argument.
-        vim.api.nvim_create_autocmd('StdinReadPre', {
-          pattern = '*',
-          callback = function()
-            vim.s.std_in = 1
-          end,
-        })
-      end,
-    },
-  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
@@ -1033,12 +1012,12 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -10,11 +10,43 @@ return {
     'MunifTanjim/nui.nvim',
   },
   cmd = 'Neotree',
+  init = function()
+    vim.api.nvim_create_autocmd('BufEnter', {
+      -- make a group to be able to delete it later
+      group = vim.api.nvim_create_augroup('NeoTreeInit', { clear = true }),
+      callback = function()
+        local f = vim.fn.expand '%:p'
+        if vim.fn.isdirectory(f) ~= 0 then
+          vim.cmd('Neotree current dir=' .. f)
+          -- neo-tree is loaded now, delete the init autocmd
+          vim.api.nvim_clear_autocmds { group = 'NeoTreeInit' }
+        end
+      end,
+    })
+    -- keymaps
+  end,
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    {
+      '<leader>nb',
+      function()
+        vim.cmd 'Neotree buffers'
+      end,
+      mode = 'n',
+      desc = '[n]eoTree [b]uffers',
+    },
+    {
+      '<leader>nf',
+      function()
+        vim.cmd 'Neotree reveal'
+      end,
+      mode = 'n',
+      desc = '[n]eotree reveal (shows [f]iles)',
+    },
   },
   opts = {
     filesystem = {
+      hijack_netrw_behavior = 'open_current',
       window = {
         mappings = {
           ['\\'] = 'close_window',
